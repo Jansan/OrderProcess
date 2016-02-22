d2200d8f320ff4e78f943c4460143e5370239e4e
Ninib83 <Ninib_83@hotmail.com>
2016-02-22 13:14:37 +0100

    commit

---
+++ OrderProcess/src/OrderProcess/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Data.Entity;
+using OrderProcess.Models;
+
+namespace OrderProcess
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddScoped<IOrderProcessRepository, OrderProcessRepository>();
+            var ConnString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=OrderProcessDB;Integrated Security=True;Pooling=False";
+
+            services
+                .AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<OrderContext>(options =>
+                options.UseSqlServer(ConnString));
+
+
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
--- OrderProcess/src/OrderProcess/ViewModels/DisplayProductsViewModel.cs
+++ OrderProcess/src/OrderProcess/ViewModels/DisplayProductsViewModel.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OrderProcess.ViewModels
 {
-    public class ListOrderViewModel
+    public class DisplayProductsViewModel
     {
-        public int Id { get; set; }
-        [Display(Name = "Vara")]
         public string Name { get; set; }
-
-        [Display(Name = "Pris")]
         public int Price { get; set; }
+
 
     }
 }
